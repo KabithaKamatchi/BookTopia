f625130cb200233faadd120880e76f1bd92a6af2
KABITHA C <111683578+KabithaKamatchi@users.noreply.github.com>
2024-09-27 14:46:40 +0530

    CRUD_Operations (#6)
    * CHANGED: Files
    
    * CHANGED: Files

--- BookTopia/Default.aspx
+++ BookTopia/Default.aspx
@@ -1,7 +1,5 @@
 ﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="BookTopia._Default" %>
 
-<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-
-   
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server"> 
 
 </asp:Content>
--- BookTopia/Site.Master.designer.cs
+++ BookTopia/Site.Master.designer.cs
@@ -15,15 +15,6 @@ namespace BookTopia
     {
 
         /// <summary>
-        /// MainContent control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
-
-        /// <summary>
         /// btnAddBook control.
         /// </summary>
         /// <remarks>
@@ -40,5 +31,14 @@ namespace BookTopia
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnViewBooks;
+
+        /// <summary>
+        /// MainContent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
     }
 }
